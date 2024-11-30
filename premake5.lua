5da9ac16963276d31ea01910dbd47840f73f295c
insanebytes <andres.rs.gm@gmail.com>
2024-11-30 21:05:12 +0100

    cambios premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,41 +1,53 @@
 project "glfw"
 	kind "StaticLib"
 	language "C"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"glfw/include/GLFW/glfw3.h",
-		"glfw/include/GLFW/glfw3native.h",
-		"glfw/src/glfw_config.h",
-		"glfw/src/context.c",
-		"glfw/src/init.c",
-		"glfw/src/input.c",
-		"glfw/src/monitor.c",
-		"glfw/src/vulkan.c",
-		"glfw/src/window.c"
+		"include/GLFW/glfw3.h",
+    	"include/GLFW/glfw3native.h",
+    	"src/internal.h",
+    	"src/platform.h",
+    	"src/mappings.h",
+    	"src/context.c",
+    	"src/init.c",
+    	"src/input.c",
+    	"src/monitor.c",
+    	"src/platform.c",
+    	"src/vulkan.c",
+    	"src/window.c",
+    	"src/egl_context.c",
+    	"src/osmesa_context.c",
+    	"src/null_platform.h",
+    	"src/null_joystick.h",
+    	"src/null_init.c",
+    	"src/null_monitor.c",
+    	"src/null_window.c",
+    	"src/null_joystick.c"
 	}
     
 	filter "system:linux"
 		pic "On"
 
 		systemversion "latest"
-		staticruntime "On"
+		staticruntime "off"
 
 		files
 		{
-			"glfw/src/x11_init.c",
-			"glfw/src/x11_monitor.c",
-			"glfw/src/x11_window.c",
-			"glfw/src/xkb_unicode.c",
-			"glfw/src/posix_time.c",
-			"glfw/src/posix_thread.c",
-			"glfw/src/glx_context.c",
-			"glfw/src/egl_context.c",
-			"glfw/src/osmesa_context.c",
-			"glfw/src/linux_joystick.c"
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
 		}
 
 		defines
@@ -45,19 +57,22 @@ project "glfw"
 
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
+		staticruntime "off"
 
 		files
 		{
-			"glfw/src/win32_init.c",
-			"glfw/src/win32_joystick.c",
-			"glfw/src/win32_monitor.c",
-			"glfw/src/win32_time.c",
-			"glfw/src/win32_thread.c",
-			"glfw/src/win32_window.c",
-			"glfw/src/wgl_context.c",
-			"glfw/src/egl_context.c",
-			"glfw/src/osmesa_context.c"
+			"src/win32_init.c",
+			"src/win32_module.c",
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.h",
+			"src/win32_time.c",
+			"src/win32_thread.h",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
 		}
 
 		defines 
@@ -73,3 +88,4 @@ project "glfw"
 	filter "configurations:Release"
 		runtime "Release"
 		optimize "on"
+		symbols "off"
